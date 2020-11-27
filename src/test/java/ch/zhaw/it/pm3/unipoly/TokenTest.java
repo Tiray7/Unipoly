fef3780edf27353fcf6179b06e4dfdfa2d7ce4fe
shakioma <shakioma@students.zhaw.ch>
2020-11-27 09:25:39 +0100

    java code for token test

--- src/test/java/ch/zhaw/it/pm3/unipoly/TokenTest.java
+++ src/test/java/ch/zhaw/it/pm3/unipoly/TokenTest.java
@@ -8,26 +8,42 @@ import org.junit.Test;
 public class TokenTest {
     Token t_token;
 
+    /***
+     * setting the test for token class
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
         t_token = new Token(TokenType.LAB);
     }
 
+    /***
+     * test token const.
+     */
     @Test
     public void testToken() {
         assertNotNull(t_token);
     }
 
+    /***
+     * testing if getting the right token type
+     */
     @Test
     public void testGetType() {
         assertEquals(t_token.getType(), TokenType.LAB);
     }
 
+    /***
+     * test if get the right index
+     */
     @Test
     public void testGetTileIndex() {
         assertEquals(t_token.getCurrFieldIndex(), 0);
     }
 
+    /***
+     * test if it move right from index field to another
+     */
     @Test
     public void testMoveTo() {
         assertEquals(t_token.getCurrFieldIndex(), 0);
@@ -35,6 +51,10 @@ public class TokenTest {
         assertEquals(t_token.getCurrFieldIndex(), 2);
     }
 
+    /***
+     * test if its move right from one field index
+     * to another by certain amount
+     */
     @Test
     public void testMoveBy() {
         assertEquals(t_token.getCurrFieldIndex(), 0);
