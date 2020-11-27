3546e3d696797893c77ade862d25e7b1a4550c4e
shakioma <shakioma@students.zhaw.ch>
2020-11-27 09:13:13 +0100

    add java doc for bank test class

--- src/test/java/ch/zhaw/it/pm3/unipoly/BankTest.java
+++ src/test/java/ch/zhaw/it/pm3/unipoly/BankTest.java
@@ -12,23 +12,36 @@ public class BankTest {
     Bank bank;
     Player playa;
 
+    /***
+     * setting test up before start testing
+     * @throws Exception
+     */
     @Before
     public void setUp() throws Exception {
         bank = new Bank();
         playa = new Player(-1, "Dilan", TokenType.EINSTEIN);
     }
 
+    /***
+     * test bank cont. with 2000000
+     */
     @Test
     public void testBank() {
         assert(bank.getMoney() == 2000000);
         assert(bank.getName().equals("Bank"));
     }
 
+    /***
+     * test free parking method
+     */
     @Test
     public void testGetFreeParking() {
         assert(bank.getFreeParking() == 100);
     }
 
+    /***
+     * test free parking award
+     */
     @Test
     public void testAwardFreeParking() {
         bank.awardFreeParking(playa);
