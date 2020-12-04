c8aba02fb014c945c05d5ae3b8a692427c7dbb99
kuendkun <kuendkun@students.zhaw.ch>
2020-12-04 15:04:07 +0100

    javaDoc: for field, fieldProperty and fieldException #81

--- src/main/java/ch/zhaw/it/pm3/unipoly/FieldProperty.java
+++ src/main/java/ch/zhaw/it/pm3/unipoly/FieldProperty.java
@@ -1,6 +1,10 @@
 package ch.zhaw.it.pm3.unipoly;
 
-public class FieldProperty extends Field{
+/**
+ * This class is for creating the property fields.
+ */
+
+public class FieldProperty extends Field {
 
     private final int propertyCost;
     private final String name;
@@ -14,6 +18,18 @@ public class FieldProperty extends Field{
     private int ownerIndex;
     private static final int UNOWNED = -1;
 
+    /** Constructor for property field
+     *
+     * @param name of the property
+     * @param label of the field, in this case PROPERTY
+     * @param propertyCost is the cost of the property
+     * @param rentLV1 the rent that you start with after acquiring the property
+     * @param rentLV2 the rent after the property levels up once
+     * @param rentLV3 the rent after the property levels up twice
+     * @param rentLV4 the rent after the property levels up thrice
+     * @param rentLV5 the rent after the property is leveled up to the maximum
+     * @param moduleGroup is which the property belongs too
+     */
     public FieldProperty(String name, Config.FieldLabel label, int propertyCost,
                          int rentLV1, int rentLV2, int rentLV3, int rentLV4, int rentLV5, int moduleGroup) {
         super(label, "");
@@ -30,14 +46,23 @@ public class FieldProperty extends Field{
     }
 
     public String getName() { return name; }
+
     public int getPropertyCost() { return propertyCost; }
+
     public int getOwnerIndex() { return ownerIndex; }
+
     public int getCurrentRent() { return currentRent; }
+
     public int getRentLV1() { return rentLV1; }
+
     public int getRentLV2() { return rentLV2; }
+
     public int getRentLV3() { return rentLV3; }
+
     public int getRentLV4() { return rentLV4; }
+
     public int getRentLV5() { return rentLV5; }
+
     public int getModuleGroup(){ return moduleGroup; }
 
     public void setOwnerIndex(int ownerIndex) {
@@ -45,9 +70,10 @@ public class FieldProperty extends Field{
     }
 
     public boolean isOwnerBank() {
-       return ownerIndex == -1;
+        return ownerIndex == -1;
     }
 
+    /**This method levels up the rent of the property when called.**/
     public void raiseRent() {
         if (currentRent == rentLV1) {
             currentRent = rentLV2;
