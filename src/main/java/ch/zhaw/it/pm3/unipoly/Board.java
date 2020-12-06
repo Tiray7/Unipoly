522418f16b274a279a9a64461ff9e3a7fb2df5bf
shakioma <shakioma@students.zhaw.ch>
2020-12-06 13:48:23 +0100

    board class Javadoc

--- src/main/java/ch/zhaw/it/pm3/unipoly/Board.java
+++ src/main/java/ch/zhaw/it/pm3/unipoly/Board.java
@@ -10,22 +10,43 @@ public class Board {
     private final Map<Integer, Field> fields;
     private final Map<Integer, FieldProperty> properties;
 
+    /***
+     * Board contractor
+     */
     public Board() {
         fields = Config.getInitialBoard();
         properties = new HashMap<>();
         fillPropertyMap();
     }
 
+    /***
+     * getFieldTypeAtIndex methode to find out the index label from teh index number
+     * @param index field index
+     * @return index and label of the field
+     * @throws FieldIndexException
+     */
     public Config.FieldLabel getFieldTypeAtIndex(int index) throws FieldIndexException {
         checkFieldIndex(index);
         return fields.get(index).getLabel();
     }
 
+    /***
+     * getFieldPropertyAtIndex method to get which property in this field index
+     * @param index field index
+     * @return property in this field index
+     * @throws FieldIndexException
+     */
     public FieldProperty getFieldPropertyAtIndex(int index) throws FieldIndexException {
         checkFieldIndex(index);
         return properties.get(index);
     }
 
+    /***
+     * getFieldAtIndex methode to get what is the field on this index
+     * @param index field index
+     * @return the field in the certain index
+     * @throws FieldIndexException
+     */
     public Field getFieldAtIndex(int index) throws FieldIndexException {
         checkFieldIndex(index);
         return fields.get(index);
@@ -60,6 +81,12 @@ public class Board {
         return properties.get(index).getOwnerIndex();
     }
 
+    /***
+     * checkFieldIndex method to check the index
+     * and find if its right
+     * @param index field index
+     * @throws FieldIndexException
+     */
     private void checkFieldIndex(int index) throws FieldIndexException {
         if (index < Config.FIELD_MIN) {
             throw new FieldIndexException("Field index ist too small");
@@ -68,6 +95,9 @@ public class Board {
         }
     }
 
+    /***
+     * fillPropertyMap method , void method to fill field property field
+     */
     void fillPropertyMap() {
         for (Map.Entry<Integer, Field> entry : fields.entrySet())
             if (entry.getValue().getLabel().equals(Config.FieldLabel.PROPERTY)) {
