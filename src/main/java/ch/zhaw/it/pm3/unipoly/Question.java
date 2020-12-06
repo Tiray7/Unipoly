0417332bed1c1a9c949043b00858a0fd08f3c4e9
kuendkun <kuendkun@students.zhaw.ch>
2020-12-06 19:45:24 +0100

    fix: correct bug

--- src/main/java/ch/zhaw/it/pm3/unipoly/Question.java
+++ src/main/java/ch/zhaw/it/pm3/unipoly/Question.java
@@ -7,6 +7,7 @@ import java.util.Hashtable;
  */
 public class Question {
     String question;
+    Hashtable<Integer, String> answerOptions = new Hashtable<>();
     int solution;
     String option1;
     String option2;
@@ -27,12 +28,18 @@ public class Question {
         this.option1 = option1;
         this.option2 = option2;
         this.option3 = option3;
+        answerOptions.put(1, this.option1);
+        answerOptions.put(2, this.option2);
+        answerOptions.put(3, this.option3);
     }
 
     public String getQuestion() {
         return question;
     }
 
+    public Hashtable<Integer, String> getAnswerOptions() {
+        return answerOptions;
+    }
 
     public int getSolution() {
         return solution;
